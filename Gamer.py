0b4673caa6c880e111de86116c22b2699b6772d5
nowij3 <41338377+nowij3@users.noreply.github.com>
2019-03-28 14:37:27 +0900

    Update Gamer.py

--- Gamer.py
+++ Gamer.py
@@ -6,19 +6,19 @@ _chip_choice           # 코인 선택
 _balance               # 가진 금액
 INIT_MONEY = 10000     # 시작금
 
-hand                  # 손에 든 카드
-hand_num              # 가진 카드 개수
-hand_sum              # 가진 카드 합
+_hand                  # 손에 든 카드
+_hand_num              # 가진 카드 개수
+_hand_sum              # 가진 카드 합
 _counting              # 카운팅 결과 합
 
-play_status           # 현재 게임 상태 (st_hit, st_stand, st_bust)
+_play_status           # 현재 게임 상태 (st_hit, st_stand, st_bust)
 _money_status          # 현재 재산 상태 (True, False)
 
 def __init__(self, deck) :
     self._balance = self.INIT_MONEY
-    self.hand = []
-    self.hand_num = 0
-    self.hand_sum = 0
+    self._hand = []
+    self._hand_num = 0
+    self._hand_sum = 0
     self._counting = 0
     self._play_status = "st_hit"
     self._money_status = True
@@ -40,23 +40,23 @@ def deal(self) :
         self._balance -= self._chip_choice
 
         # 카드 두 장 뒤집기
-        self.hand[self.hand_num] = get_card()
-        self.hand_num += 1
-        self.hand[self.hand_num] = get_card()
-        self.hand_num += 1
+        self._hand[self._hand_num] = get_card()
+        self._hand_num += 1
+        self._hand[self._hand_num] = get_card()
+        self._hand_num += 1
 
-        return hand
+        return self._hand
     
 def open_card() :
     return
 
 def stand(self) :
     self._play_status == "st_stand"
-    return play_status
+    return self._play_status
    
 def hit(self) :
     if self._play_status == "st_hit" :
-        self.hand[self.hand_num] = get_card()
-        self.hand_num += 1
-        self.open_card()
+        self._hand[self._hand_num] = get_card()
+        self._hand_num += 1
+        self._open_card()
         
