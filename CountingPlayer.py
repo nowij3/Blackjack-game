4306313a710d43e49fffb737317a4e766e3dea84
nowij3 <41338377+nowij3@users.noreply.github.com>
2019-04-12 17:30:18 +0900

    Update CountingPlayer.py

--- CountingPlayer.py
+++ CountingPlayer.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 import random
-import Gamer
+from Gamer import Gamer
 import CountingList
 
 class CountingPlayer(Gamer):
     def __init__(self, name):
-        super.__init__(self)
+        super().__init__()
 
         # GUI를 위해 알고리즘 플레이어 이름 부여
         self.name = name
         
-        self.count_list = super.select_count_list(self.name)
+        self.count_list = super().select_count_list(self.name)
         
     # 카운팅 알고리즘 적용
     def card_count(self):
@@ -22,7 +22,6 @@ class CountingPlayer(Gamer):
             if self.count_list[i][0] == self.hand[self.hand_num-1][0]:
                 self.hand_count += self.count_list[i][1]
                 break
-            i += 1
 
     # 카운팅 알고리즘을 적용한 값 반환
     def get_hand_count(self) :
