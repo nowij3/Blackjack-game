baa593644b4000e1f9a1e19d054466968fd63df2
gyeong-ah <43920753+gyeong-ah@users.noreply.github.com>
2019-04-29 11:12:42 +0900

    Update Dealer.py

--- Dealer.py
+++ Dealer.py
@@ -4,6 +4,11 @@ import Deck as deck
 
 class Dealer(Gamer):
     # 수정해야댐
+    ''' 
+        처음 한장을 공개하고, 다른플레이어들이 결정을 모두 한 이후에 마지막으로 딜러가 카드를 공개함
+        그래서 지금 당장 카드를 한장밖에 공개안한상태라 17이 될수가없음
+        두장째 공개한 이후에 play를 해야되는뎀
+    '''
     def open_deal_card(self):
         # 카드 합 계산
         self.hand_sum += deck[0][2]
