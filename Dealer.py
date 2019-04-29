4c5835ea33e5b9e5d50363439cd5c59acdd8ddd8
gyeong-ah <43920753+gyeong-ah@users.noreply.github.com>
2019-04-29 11:02:53 +0900

    Update Dealer.py

--- Dealer.py
+++ Dealer.py
@@ -3,18 +3,10 @@ import Deck as deck
 
 
 class Dealer(Gamer):
-    
-    def __init__(self):
-        self._hand = []  # 가지고 있는 카드
-        self._hand_num = 0  # 가지고 있는 카드 개수
-        self._hand_sum = 0  # 가지고 있는 카드의 숫자 합
-        self._play_status = 'st_hit'  # 현재 게임 상태 [st_hit, st_stand, st_bust]
-        self._blackjack = False
-    
+
     def open_deal_card(self):
         # 카드 합 계산
-        for i in range(2):
-            self.hand_sum += deck[i][2]
+        self.hand_sum += deck[0][2]
 
         # Ace가 두 장일 경우 합계
         if self.hand_sum > 21:
@@ -24,3 +16,10 @@ class Dealer(Gamer):
     def play(self):
         while self.hand_sum < 17:
             self.hit()
+
+    # 게임 가능 여부 확인 -
+    def is_playable(self):
+        if self.play_status == 'st_stand':
+            return True
+        return False
+
