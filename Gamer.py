079c4a9f062547522766d44d01b922da13037d39
gyeong-ah <43920753+gyeong-ah@users.noreply.github.com>
2019-04-29 11:01:39 +0900

    Update Gamer.py

--- Gamer.py
+++ Gamer.py
@@ -15,10 +15,7 @@ import random
 ##############################################################################################
 
 #############################################################
-# new_hand안에 self.blackjack = false했는데 ㅇㅋ?
-# is_bettable 혹시 has_money로 해도 괜찮은가염 아님 haschip이라든가 bettable 파이참이랑 깉헙ㅇ ㅣ없는단어라고 머라함ㅠㅠ
-# is_blackjack을 deal함수 내에서 호출하는게아니라 메인에서 deal한 다음에 호출하는게 맞을것같은..?
-# 생각해보니 blackjack이 상태인건 좀 이상한것같은데 그냥 is_blackjack에서 true false리턴하는게 
+# deck, playing_deck 구별
 #############################################################
 
 
@@ -26,24 +23,15 @@ class Gamer:
     INIT_MONEY = 10000  # 시작금
 
     def __init__(self):
-        self._chip_choice = None  # 선택한 코인
-        self._balance = self.INIT_MONEY  # 가진 금액
         self._hand = []  # 가지고 있는 카드
         self._hand_num = 0  # 가지고 있는 카드 개수
         self._hand_sum = 0  # 가지고 있는 카드의 숫자 합
-        self._counting = 0  # 카운팅 합산
         self._play_status = 'st_hit'  # 현재 게임 상태 [st_hit, st_stand, st_bust]
-        self._money_status = True  # 현재 재산 상태 [ True, False (베팅가능, 불가능) ]
         self._blackjack = False
 
     # 게임 재시작. 게임 전체 초기화
     def new_game(self):
         # 전체변수 재설정
-        self.chip_choice = None
-        self.balance = self.INIT_MONEY
-        self.counting = 0
-        self.money_status = True
-
         self.new_hand()
 
     def new_hand(self):
@@ -57,60 +45,39 @@ class Gamer:
         self.deal()
 
     # 카드 받기
-    def get_card(self, deck):
+    def get_card(self, playing_deck):
+        ################################
+        # deck reset 적용할때 수정
+        ################################
         while True:
             rand_num = random.randint(0, 13)  # 카드 랜덤 선택 (13장 카드 기준)
-            if deck[rand_num][1] > 0:  # deck에 해당 카드가 없으면 다시 뽑기
+            if playing_deck[rand_num][1] > 0:  # deck에 해당 카드가 없으면 다시 뽑기
                 break
-        deck[rand_num][1] -= 1  # 뽑은 카드 한장을 덱에서 제외
-        rand_card = deck[rand_num]  # 뽑은 카드 기억
+
+        playing_deck[rand_num][1] -= 1  # 뽑은 카드 한장을 덱에서 제외
+        rand_card = playing_deck[rand_num]  # 뽑은 카드 기억
         card = [rand_card[0], rand_card[1]]  # 카드의 모양과 숫자 기억
         return card  # Suit, Denomination
 
     # 딜
-    def deal(self):
-        if self.money_status == True:
+    def deal(self, playing_deck):
+        if self.money_status:
             self.balance -= self.chip_choice
 
             # 카드 두 장 뒤집기
-            self.hand[self.hand_num] = self.get_card()
+            self.hand[self.hand_num] = self.get_card(playing_deck)
             self.hand_num += 1
-            self.hand[self.hand_num] = self.get_card()
+            self.hand[self.hand_num] = self.get_card(playing_deck)
             self.hand_num += 1
 
         return self.hand
 
     # 카드 공개
     def open_card(self):
-        num_of_A = 0
 
         # Hit으로 카드 공개
         if self.hand_num > 2:
-
-            # setImage
-
-            # calculate counting
-            for i in range(13):  # 카운팅 리스트 전체에서 반복 (어떤 카운팅 리스트이든 카드는 13장으로 고정)
-                if self.count_list[i][0] == str(self.hand[-1][1]):  # count_list에는 숫자-점수, hand에는 모양-숫자
-                    self.counting += self.count_list[i][1]  # 일단 running count // true count로 바꾸기
-                    break
-
-            ####################
-            # sum card numbers #
-            ####################
-            for check in self.hand:  # 가지고있는 카드에서 A개수 확인
-                if 'A' in check:
-                    num_of_A += 1
-
-            for i in range(len('''deck''')):  # deck에서 숫자에 해당하는 값을 찾아서 더함
-                if str(self.hand[-1][1]) == deck[i][0]:  # deck에는 숫자-개수-값-모양
-                    self.hand_sum += deck[i][2]
-
-            if num_of_A > 0:  # A를 가지고있는 경우
-                self.hand_sum = self.decide_ace_point(num_of_A)
-
-            self.is_bust()      # bust 확인
-
+            self.open_hit_card()
 
         # 처음 Deal로 카드를 두장 받은상태
         elif self.hand_num == 2:
@@ -130,11 +97,9 @@ class Gamer:
     # Stand. 카드 더이상 받지 않음
     def stand(self):
         self._play_status = "st_stand"
-        return self._play_status
 
     # Ace 카드 점수 결정
     ''' deck에서 A값을 11로 설정하고 합이 21을 넘을경우 10을빼서 1로 만드는방법으로'''
-
     def decide_ace_point(self, num_of_ace):
         if self.hand_sum > 21:
             for i in range(num_of_ace):
@@ -144,11 +109,10 @@ class Gamer:
 
         return self.hand_sum
 
-    # 딜에서 카드 받은 경우 - 딜러클래스 별도구현 용이하게 따로만듦 >> polymorphism.. 사실 딜러클래스에서는 그냥 반복만 안하면되는...
+    # 딜에서 카드 받은 경우
     def open_deal_card(self):
         for i in range(2):
             # setImage
-            self.counting += self.count_list[i][1]  # 카운팅 점수 계산
             self.hand_sum += deck[i][2]
 
         if self.hand_sum > 21:
@@ -158,15 +122,29 @@ class Gamer:
 
         self.is_blackjack()
 
-    # 다른 플레이어가 카드를 받았을 때 나의 hit/stand 여부와 상관없이 카운팅 계산에 반영하는 함수
-    def others_card(self, o_card):
-        # o_card == [모양, 숫자]
-        # mycountlist == (e.g.)Zen[[숫자, 값], ... ]
+    def open_hit_card(self):
+        num_of_A = 0
 
-        for i in range(12):
-            # o_card의 숫자에 해당하는 카드 카운팅 적용값 찾고, 카운팅 변수에 더하기
-            if o_card[1] == self.count_list[i][0]:
-                self.counting += self.count_list[i][1]   # 여기 counting을 각각의 플레이어에게 전달해야하지않나여
+        # setImage
+
+        ####################
+        # sum card numbers #
+        ####################
+        for check in self.hand:  # 가지고있는 카드에서 A개수 확인
+            if 'A' in check:
+                num_of_A += 1
+
+        ########################################################################################################
+        ############# 여기 len(Deck) 수정 #######################################################################
+        ########################################################################################################
+        for i in range(len(DECK)):  # deck에서 숫자에 해당하는 값을 찾아서 더함
+            if str(self.hand[-1][1]) == deck[i][0]:  # deck에는 숫자-개수-값-모양
+                self.hand_sum += deck[i][2]
+
+        if num_of_A > 0:  # A를 가지고있는 경우
+            self.hand_sum = self.decide_ace_point(num_of_A)
+
+        self.is_bust()  # bust 확인
 
     # 합이 21을 초과하는지 검사하고 초과하면 play_status 변경
     def is_bust(self):
@@ -177,9 +155,11 @@ class Gamer:
     def is_blackjack(self):
         if self.hand_num == 2 and self.hand_sum == 21:
             self.blackjack = True
+            return True
+        return False
 
     # 잔고 확인
-    def is_bettable(self):
+    def has_money(self):
         if self.balance < 100:
             self.money_status = False
             return False
@@ -187,7 +167,7 @@ class Gamer:
 
     # 게임 가능 여부 확인 -
     def is_playable(self):
-        if self.is_bettable() and self.play_status == 'st_stand':
+        if self.has_money() and self.play_status == 'st_stand':
             return True
         return False
 
