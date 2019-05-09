536cbd9e017a9b9d75367bb16b572b158444cb2a
nowij3 <nowij3@hanmail.net>
2019-05-09 19:16:26 +0900

    Update PerformanceComparisonTest.py

--- PerformanceComparisonTest.py
+++ PerformanceComparisonTest.py
@@ -24,13 +24,12 @@ def get_prize(winner_list) :
         # 게임에 참여한 경우에만
         if player_list[i].has_money() :
             player_list[i].balance -= player_list[i].chip_choice
-    
+
     for i in range(len(winner_list)) :
         if winner_list[i].name == 'Dealer' :
             continue
         else :
             winner_list[i].balance += prize_chip(winner_list[i])
-            winner_list[i].num_of_winning += 1
 
 # hit 상태인 플레이어가 남아있는지 확인
 def hit_anyone() :
@@ -179,17 +178,19 @@ def play_start() :
 
 # 딜
 def play_deal() :  
-    
+
     # deck에 8장 이하만 남으면 덱을 초기화
     if deck_handler.get_remaining_card() <= 0.5 :
         deck_handler.reset()
-
+        for i in range(5) :
+            player_list[i].counting = 0
+            
     # hit 가능한 상태로 초기화
     dealer.play_status = "st_hit"
     for i in range(5) :
         if player_list[i].has_money() :
             player_list[i].play_status = "st_hit"
-
+            
     # deal
     dealer.deal()
     for i in range (5) :
@@ -198,7 +199,7 @@ def play_deal() :
 
     # dealer.open_deal_card()
     give_my_card_info(3, dealer.hand[0])
-    
+
     for i in range (5) :
         # 카드를 받았으면 (이 부분 수정해야되나?)
         if player_list[i].hand :
@@ -208,34 +209,38 @@ def play_deal() :
 
     play_continue()
 
-# 딜 이후 게임 진행 
+# 딜 이후 게임 진행  ***이 지점에서 문제 발생***
 def play_continue() :
-
     if hit_anyone() :
         play_hit()
-
+        
     # 모두의 hit가 끝나면
     # 딜러의 딜에서 받은 뒤집히지 않은 카드 오픈
     dealer.open_second_card()
     give_my_card_info(5, dealer.hand[1])
-
+    
     # 딜러가 카드를 hit 할 때 마다 다른 플레이어들에게 카드 정보 주기
     while dealer.make_decision() :
         give_my_card_info(5, dealer.hand[-1])
 
     ## final_information()    
     check_blackjack()
+
     play_round_end()
+
 
 # 히트
 def play_hit() :
-        
+  
     while hit_anyone() :
-        
         for i in range(5) :       
             if player_list[i].is_playable() :
+
+                # ***이 지점에서 문제 발생***
                 if player_list[i].make_decision() :
                     give_my_card_info(i, player_list[i].hand[-1])
+                
+
 
 # 한 라운드 종료
 def play_round_end() :
@@ -257,10 +262,10 @@ def play_game_end(test_case) :
 
     print("\n***GAME END***")
     print("total round : ", test_case)
-    print("num of winning and winning rate")
+    print("num of winning and winning rate and balance")
     for i in range(5) :
-        print(player_list[i].name,":",player_list[i].num_of_winning,",", int((player_list[i].num_of_winning/test_case) * 100), "%")
-    
+        print(player_list[i].name,":",player_list[i].num_of_winning,",", int((player_list[i].num_of_winning/test_case) * 100), "%,",player_list[i].balance)
+    print(" ")
     
 # 메인 테스트 함수
 def final_information() :
@@ -273,7 +278,7 @@ def routine(test_case) :
     play_new_game()
     
     for i in range(test_case) :
-        print("round : ", i+1)
+        ## print("round : ", i+1)
         play_new_hand()
 
     # 실행 횟수를 채우면
@@ -290,4 +295,4 @@ dealer = Dealer.Dealer()
 player_list = []
 winner_list = []
 
-routine(8)
+routine(15)
